68db8e64de7c78785dd58bdf2f91d058738bfd04
Jake Soenneker <jake@soenneker.com>
2023-03-31 20:25:14 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.Enumerable.Tuple.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Enumerable.Tuple/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.enumerable.tuple/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.enumerable.tuple/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.Enumerable.Tuple.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Enumerable.Tuple/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Extensions.Enumerable.Tuple
+### A collection of useful IEnumerable<Tuple> extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.Enumerable.Tuple
+```
---
+++ src/EnumerableTupleExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Soenneker.Extensions.Enumerable.Tuple;
+
+public static class EnumerableTupleExtension
+{
+    /// <summary>
+    /// Checks if the first value and second value equal any of the tuples (item1 & item2) in the enumerable
+    /// </summary>
+    public static bool ContainsItem<TFirst, TSecond>(this IEnumerable<Tuple<TFirst, TSecond>> value, TFirst item1, TSecond item2)
+    {
+        return value.Any(x => x.Item1!.Equals(item1) && x.Item2!.Equals(item2));
+    }
+
+    /// <summary>
+    /// Checks if the first item and second item in the tuple equal any of the tuples (item1 & item2) in the enumerable
+    /// </summary>
+    public static bool ContainsItem<TFirst, TSecond>(this IEnumerable<Tuple<TFirst, TSecond>> value, Tuple<TFirst, TSecond> item)
+    {
+        return value.Any(x => x.Item1!.Equals(item.Item1) && x.Item2!.Equals(item.Item2));
+    }
+}

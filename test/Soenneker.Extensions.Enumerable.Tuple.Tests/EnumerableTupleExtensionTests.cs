defe41817ae771bc61d77bd81cdca57da5dcadd8
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:20 +0000

    Tests

---
+++ test/Soenneker.Extensions.Enumerable.Tuple.Tests/EnumerableTupleExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Enumerable.Tuple.Tests;
+
+public class EnumerableTupleExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}

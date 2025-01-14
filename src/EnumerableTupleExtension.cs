80b211f90645e064795aa823e1fbf7ce4f021042
Jake Soenneker <jake@soenneker.com>
2025-01-14 10:43:35 -0600

    Perf and allocation improvements

--- src/EnumerableTupleExtension.cs
+++ src/EnumerableTupleExtension.cs
@@ -9,16 +9,42 @@ public static class EnumerableTupleExtension
     /// <summary>
     /// Checks if the first value and second value equal any of the tuples (item1 & item2) in the enumerable
     /// </summary>
-    public static bool ContainsItem<TFirst, TSecond>(this IEnumerable<Tuple<TFirst, TSecond>> value, TFirst item1, TSecond item2)
+    public static bool ContainsItem<TFirst, TSecond>(this IEnumerable<Tuple<TFirst, TSecond>> source, TFirst item1, TSecond item2)
     {
-        return value.Any(x => x.Item1!.Equals(item1) && x.Item2!.Equals(item2));
+        using IEnumerator<Tuple<TFirst, TSecond>> enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            Tuple<TFirst, TSecond> current = enumerator.Current;
+
+            if (EqualityComparer<TFirst>.Default.Equals(current.Item1, item1) &&
+                EqualityComparer<TSecond>.Default.Equals(current.Item2, item2))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /// <summary>
     /// Checks if the first item and second item in the tuple equal any of the tuples (item1 & item2) in the enumerable
     /// </summary>
-    public static bool ContainsItem<TFirst, TSecond>(this IEnumerable<Tuple<TFirst, TSecond>> value, Tuple<TFirst, TSecond> item)
+    public static bool ContainsItem<TFirst, TSecond>(this IEnumerable<Tuple<TFirst, TSecond>> source, Tuple<TFirst, TSecond> item)
     {
-        return value.Any(x => x.Item1!.Equals(item.Item1) && x.Item2!.Equals(item.Item2));
+        using IEnumerator<Tuple<TFirst, TSecond>> enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            Tuple<TFirst, TSecond> current = enumerator.Current;
+
+            if (EqualityComparer<TFirst>.Default.Equals(current.Item1, item.Item1) &&
+                EqualityComparer<TSecond>.Default.Equals(current.Item2, item.Item2))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
